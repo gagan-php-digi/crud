32d44618d04052ba9d847972a410a7167ddedbfe
gagan-php-digi <gagandeepkaurchabhal@gmail.com>
2024-08-26 11:16:34 +0530

    add changes

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,16 +14,14 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
+  public function edit(Request $request): View
     {
         return view('profile.edit', [
             'user' => $request->user(),
         ]);
-    }
+     }
 
-    /**
-     * Update the user's profile information.
-     */
+   
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $request->user()->fill($request->validated());
